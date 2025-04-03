004662c64f3e523acd9d23a6172a05c353b84108
hosef <joseph@flattandsons.com>
2025-04-03 13:37:04 -0700

    Initial work on creating the recipe builder UI

---
+++ README.md
@@ -0,0 +1,16 @@
+Recipe Builder
+========
+
+This module provides tools and a UI for creating modules that bundle prebuilt
+config and content for easy reuse.
+
+License
+-------
+
+This project is GPL v2 software. See the LICENSE.txt file in this directory for
+complete text.
+
+Maintainers
+-----------
+
+- Joseph Flatt (https://github.com/hosef/)
---
+++ inc/Recipe.php
@@ -0,0 +1,29 @@
+<?php
+
+class Recipe {
+  public $name;
+  public $path;
+  public $status;
+
+  public $info = [];
+
+  // Config
+  // Content
+  // Styles
+  public $components = [];
+
+  public function get_components() {
+    if (empty($this->componenets)) {
+      // idk
+    }
+
+    return $this->componenets;
+  }
+
+  public function get_info() {}
+  public function add_dependency($module) {
+    if (!in_array($module, $this->info['dependencies'])) {
+      $this->info['dependencies'][] = $module;
+    }
+  }
+}
